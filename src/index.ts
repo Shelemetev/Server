be587ac4475fa31aecbd811b2c85efe26d90b601
Egor <egor.shelemetev228@mail.ru>
2025-02-26 12:38:54 +1000

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from "express";
 
 const app : any = express()
-const port : string = process.env.PORT ;
+const port: 3000  = 3000;
 app.get('/', (req : any, res : any): any => {
     res.send('Hello World')
 })
